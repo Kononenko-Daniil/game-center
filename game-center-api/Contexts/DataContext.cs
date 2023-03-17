ab6a7ac418fbb014d0d68053519274f135692392
Kononenko-Daniil <neokv210@gmail.com>
2023-03-17 18:38:07 +0300

    Pretifying

--- game-center-api/Contexts/DataContext.cs
+++ game-center-api/Contexts/DataContext.cs
@@ -1,23 +1,48 @@
 ﻿using game_center_api.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Reflection;
+using System.Reflection.Metadata;
 
 namespace game_center_api.Contexts
 {
     public class DataContext : DbContext
     {
         protected readonly IConfiguration Configuration;
+        public DbSet<Result> Results { get; set; }
 
         public DataContext(IConfiguration configuration)
         {
             Configuration = configuration;
+            Database.EnsureCreated();
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            // connect to sqlite database
-            options.UseSqlite(Configuration.GetConnectionString("WebApiDatabase"));
+            optionsBuilder.UseSqlite(Configuration.GetConnectionString("WebApiDatabase"), options =>
+            {
+                options.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+            });
+
+            base.OnConfiguring(optionsBuilder);
         }
 
-        public DbSet<Result> Results { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Result>().ToTable("Results", "test");
+            modelBuilder.Entity<Result>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.UserId).IsRequired();
+                entity.Property(e => e.UserFirstName).IsRequired();
+                entity.Property(e => e.UserSecondName).IsRequired();
+                entity.Property(e => e.GameId).IsRequired();
+                entity.Property(e => e.ResultValue).IsRequired();
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        
     }
 }
--- game-center-api/Models/Result.cs
+++ game-center-api/Models/Result.cs
@@ -1,12 +1,21 @@
-﻿namespace game_center_api.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace game_center_api.Models
 {
     public class Result
     {
+        [Key, NotNull, Required]
         public int Id { get; set; }
+        [Required, NotNull]
         public string UserId { get; set; }
+        [Required, NotNull]
         public string UserFirstName { get; set; }
+        [Required, NotNull]
         public string UserSecondName { get; set; }
+        [Required, NotNull]
         public string GameId { get; set; }
+        [Required, NotNull]
         public int ResultValue { get; set; }
     }
 }
